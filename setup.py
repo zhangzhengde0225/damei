0c3f97818393b880b31628a3f6b193fefa0835d9
zhangzhengde0225 <drivener@163.com>
2020-12-30 11:42:11 +0800

    zzd commit2

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ URL = 'https://github.com/zhangzhengde0225/damei'
 EMAIL = 'drivener@163.com'
 AUTHOR = 'Zhengde Zhang'
 REQUIRES_PYTHON = '>=3.6.0'
-VERSION = '1.0.2'
+VERSION = '1.0.3'
 
 # What packages are required for this module to be executed?
 REQUIRED = [
